5b6e44abf56a50b9438f054985a10868fada7773
vinhnguyen2110 <57150520+quangvinh2110@users.noreply.github.com>
2025-02-10 17:50:04 +0700

    Update evaluate_llm_async.sh

--- evaluate_llm_async.sh
+++ evaluate_llm_async.sh
@@ -3,6 +3,6 @@
 python3 evaluate_llms_async.py \
 --endpoint_ip "http://0.0.0.0:8264" \
 --served_model_name "Vi-Llama-3.3-70B-Instruct" \
---model_path "/raid/HUB_LLM/010225_llama33_70b_instruct/checkpoint-1470" \
+--tokenizer_path "/raid/HUB_LLM/010225_llama33_70b_instruct/checkpoint-1470" \
 --engine "vllm" \
 --dataset_name "vmlu" 
